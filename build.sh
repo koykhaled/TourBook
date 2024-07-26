738d5f7614ee57603a7c2e021692352cb4ccebdc
Takla Zidan <ztakla3@gmail.com>
2024-07-26 20:13:12 +0300

    edit bash.sh

--- build.sh
+++ build.sh
@@ -1,22 +1,41 @@
+# #!/usr/bin/env bash
+# # Exit on error
+# set -o errexit
+
+# # Modify this line as needed for your package manager (pip, poetry, etc.)
+# pip install -r requirements.txt
+
+# # Convert static asset files
+# python manage.py collectstatic --no-input
+
+# # Apply any outstanding database migrations
+# # python manage.py migrate
+# #!/bin/bash
+
+# # Apply database migrations
+# echo "Applying database migrations..."
+# python manage.py makemigrations
+# python manage.py migrate
+
+# # Start the application
+# echo "Starting the application..."
+# gunicorn TourBook.asgi:application --workers=4 --worker-class=uvicorn.workers.UvicornWorker --bind=0.0.0.0:$PORT
 #!/usr/bin/env bash
-# Exit on error
+# exit on error
 set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt
+poetry install
 
-# Convert static asset files
+/opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
+/opt/render/project/src/.venv/bin/python -m pip install -r requirements.txt
+/opt/render/project/src/.venv/bin/python manage.py collectstatic --no-input
+/opt/render/project/src/.venv/bin/python manage.py migrate
+
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+poetry install
+
 python manage.py collectstatic --no-input
-
-# Apply any outstanding database migrations
-# python manage.py migrate
-#!/bin/bash
-
-# Apply database migrations
-echo "Applying database migrations..."
-python manage.py makemigrations
 python manage.py migrate
-
-# Start the application
-echo "Starting the application..."
-gunicorn TourBook.asgi:application --workers=4 --worker-class=uvicorn.workers.UvicornWorker --bind=0.0.0.0:$PORT
